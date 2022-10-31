13407381b0607d336c91f6b4e3c7ae88ea68474a
LuK1337 <priv.luk@gmail.com>
2022-10-31 21:59:52 +0100

    vili: overlay-lineage: Configure aux camera for Aperture
    Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # RRO Overlays
 PRODUCT_PACKAGES += \
