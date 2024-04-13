da9cfbf2861ae9556f8ef5a6efb98c6d515160ec
swiitchOFF <bmk.bishu0075@gmail.com>
2024-04-13 15:24:16 +0530

    vili: Update blobs to V14.0.18.0.TKDEUXM

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -26,9 +26,9 @@ PRODUCT_SYSTEM_NAME := vili_global
 PRODUCT_SYSTEM_DEVICE := vili
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vili-user 13 RKQ1.211001.001 V14.0.16.0.TKDEUXM release-keys" \
+    PRIVATE_BUILD_DESC="vili-user 13 RKQ1.211001.001 V14.0.18.0.TKDEUXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/vili/vili:13/RKQ1.211001.001/V14.0.16.0.TKDEUXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/vili/vili:13/RKQ1.211001.001/V14.0.18.0.TKDEUXM:user/release-keys
